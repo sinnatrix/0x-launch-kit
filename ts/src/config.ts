d97551a45ed2c423e373bb305a2505e74ab3f766
sinnatrix <31360025+sinnatrix@users.noreply.github.com>
2018-12-15 20:43:34 +0100

    Update config.ts

--- ts/src/config.ts
+++ ts/src/config.ts
@@ -12,7 +12,7 @@ export const MAX_PER_PAGE = 1000;
 // Default network id to use when not specified
 export const NETWORK_ID = 42;
 // An array of fee recipients
-export const FEE_RECIPIENT = '0x0000000000000000000000000000000000000000';
+export const FEE_RECIPIENT = '0x858Fb353c8d3c72B0a9e6DFAbc78a0A6cF083bB1';
 // A flat fee in ZRX that should be charged to the order maker
 export const MAKER_FEE_ZRX_UNIT_AMOUNT = new BigNumber(0);
 // A flat fee in ZRX that should be charged to the order taker
@@ -21,8 +21,9 @@ export const TAKER_FEE_ZRX_UNIT_AMOUNT = new BigNumber(0);
 export const WHITELISTED_TOKENS: string[] | '*' = [
     '0x2002d3812f58e35f0ea1ffbf80a75a38c32175fa', // ZRX
     '0xd0a1e359811322d97991e03f863a0c30c2cf029c', // WETH
+    '0x66142B81db17d7c0bd91f502D00382e326a24c2a', // GEX
 ];
 // Ethereum RPC url
-export const RPC_URL = 'https://kovan.infura.io/v3';
+export const RPC_URL = 'https://mainnet.infura.io/v3';
 // Default ERC20 token precision
 export const DEFAULT_ERC20_TOKEN_PRECISION = 18;
